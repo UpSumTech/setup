562da73ded869a6fb3ac23bba83196b91a01ab09
Suman Mukherjee <sumanmukherjee03@gmail.com>
2015-06-11 00:22:31 -0700

    Fixed postgres healthcheck

--- docker/consul/postgres/healthcheck/postgres-check.sh
+++ docker/consul/postgres/healthcheck/postgres-check.sh
@@ -3,12 +3,17 @@
 set -e
 
 main() {
-  echo "$POSTGRESSERVER_PORT_5432_TCP_ADDR:5432:*:$POSTGRESSERVER_ENV_USER:$POSTGRESSERVER_ENV_PASSWD" > "/.pgpass"
-  export PGPASSFILE="/.pgpass"
+  echo "$POSTGRESSERVER_PORT_5432_TCP_ADDR:5432:*:$POSTGRESSERVER_ENV_USER:$POSTGRESSERVER_ENV_PASSWD" > "$HOME/.pgpass"
+  chmod 0600 "$HOME/.pgpass"
   local cmd=( \
     "psql" \
-    "-c" \
-    "'SHOW DATABASES;'" \
+    "-h" \
+    "$POSTGRESSERVER_PORT_5432_TCP_ADDR" \
+    "-U" \
+    "$POSTGRESSERVER_ENV_USER" \
+    "-p" \
+    "5432" \
+    "-l" \
   )
   exec ${cmd[@]}
 }
